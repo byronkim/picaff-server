2b490368eb10c4f90bfb81f4a300c12b5e23bf5c
Biron Kim <bironkim88@gmail.com>
2022-01-07 21:25:18 +0900

    checking email

--- controllers/user/mail.ts
+++ controllers/user/mail.ts
@@ -13,34 +13,36 @@ const makeSerial = (): string => {
 }
 
 const mail = async (req: Request, res: Response) => {
-const { email } = req.body
-console.log('이메일 확인중')
-console.log(email)
-  if (req.query.type !== 'signoff') {
-    const isExistEmail = await interfaces.isCheckedUser(email)
-    if (isExistEmail) return res.status(403).send({ message: '이미 사용중인 메일주소입니다.' })
-  }
+  const { email } = req.body
+  console.log('이메일 확인중')
+  let checkEmail = /^[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/
+  if (checkEmail.test(email) === true) {
+    console.log(email)
+    if (req.query.type !== 'signoff') {
+      const isExistEmail = await interfaces.isCheckedUser(email)
+      if (isExistEmail) return res.status(403).send({ message: '이미 사용중인 메일주소입니다.' })
+    }
 
-  //이미 존재하는 메일 여부 체크 유저정보에서 확인해서 예외처리
+    //이미 존재하는 메일 여부 체크 유저정보에서 확인해서 예외처리
 
-  const serialnum = makeSerial()
-  let transporter = nodemail.createTransport({
-    service: 'gmail',
-    port: 587,
-    host: 'smtp.gmail.com',
-    secure: false,
-    requireTLS: true,
-    auth: {
-      user: 'picaff.operator@gmail.com',
-      pass: process.env.MAIL_PASSWORD,
-    },
-  })
+    const serialnum = makeSerial()
+    let transporter = nodemail.createTransport({
+      service: 'gmail',
+      port: 587,
+      host: 'smtp.gmail.com',
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: 'picaff.operator@gmail.com',
+        pass: process.env.MAIL_PASSWORD,
+      },
+    })
 
-  await transporter.sendMail({
-    from: '"Picaff 당신의 커피 취향을 알아보세요" <picaff.operator@gmail.com>',
-    to: `${email}`,
-    subject: '[Picaff] email 인증요청',
-    html: `
+    await transporter.sendMail({
+      from: '"Picaff 당신의 커피 취향을 알아보세요" <picaff.operator@gmail.com>',
+      to: `${email}`,
+      subject: '[Picaff] email 인증요청',
+      html: `
     <table align="center" border="0" cellpadding="0" cellspacing="0" width="600">
     <tr>
     <td align="center" bgcolor="#F7F7F4" >
@@ -54,16 +56,19 @@ console.log(email)
     </tr>
     </table>
     `,
-    attachments: [
-      {
-        filename: 'logo1.png',
-        path: __dirname + '/logo1.png',
-        cid: 'logo1.png',
-      },
-    ],
-  })
+      attachments: [
+        {
+          filename: 'logo1.png',
+          path: __dirname + '/logo1.png',
+          cid: 'logo1.png',
+        },
+      ],
+    })
 
-  return res.status(201).send({ serialnum })
+    return res.status(201).send({ serialnum })
+  } else {
+    console.log('Invalid Email')
+    return res.status(403).send({ message: 'Invalid Email' })
+  }
 }
-
 export default mail
