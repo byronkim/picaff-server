cf0d92d01e20a6f2b177a82f78f772acce17cf6d
Biron Kim <bironkim88@gmail.com>
2022-01-10 20:39:36 +0900

    fixing crawling

--- controllers/item/crawling.ts
+++ controllers/item/crawling.ts
@@ -3,9 +3,11 @@ import Crawling from '@middleware/crawling'
 
 const crawling = async (req: Request, res: Response) => {
   try {
+    console.log(req.body)
     const data = await Crawling.getData(req.body.itemName)
     return res.status(200).send(data)
   } catch (err) {
+    console.log('crawling error')
     return res.status(403).send({ message: '정확한 정보를 입력해주세요' })
   }
 }
